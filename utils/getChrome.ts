ae468b244317ecadc95cff6a5195accb46ac2ed6
LEE GUKBEOM <tmwlak@gmail.com>
2025-07-10 08:28:40 +0900

    wip (7)

--- utils/getChrome.ts
+++ utils/getChrome.ts
@@ -4,6 +4,26 @@ import puppeteer from "puppeteer";
 import puppeteerExtra from "puppeteer-extra";
 import puppeteerExtraPluginStealth from "puppeteer-extra-plugin-stealth";
 
+require("puppeteer-extra-plugin-stealth/evasions/chrome.app");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.csi");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes");
+require("puppeteer-extra-plugin-stealth/evasions/chrome.runtime");
+require("puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow");
+require("puppeteer-extra-plugin-stealth/evasions/media.codecs");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.languages");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.permissions");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.plugins");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.vendor");
+require("puppeteer-extra-plugin-stealth/evasions/navigator.webdriver");
+require("puppeteer-extra-plugin-stealth/evasions/sourceurl");
+require("puppeteer-extra-plugin-stealth/evasions/user-agent-override");
+require("puppeteer-extra-plugin-stealth/evasions/webgl.vendor");
+require("puppeteer-extra-plugin-stealth/evasions/window.outerdimensions");
+require("puppeteer-extra-plugin-stealth/evasions/defaultArgs");
+require("puppeteer-extra-plugin-user-preferences");
+require("puppeteer-extra-plugin-user-data-dir");
+
 export async function getChrome() {
   //   console.debug("🐞process.env.VERCEL_ENV");
   //   console.debug(process.env.VERCEL_ENV);
@@ -21,11 +41,11 @@ export async function getChrome() {
   }
 
   // (Production)
-//   console.log("Running in Vercel production environment");
-//   const executablePath = await chromium.executablePath();
+  //   console.log("Running in Vercel production environment");
+  //   const executablePath = await chromium.executablePath();
 
   return {
-    executablePath : await chromium.executablePath(),
+    executablePath: await chromium.executablePath(),
     puppeteer: puppeteerExtra,
     // puppeteer: puppeteerCore, // 가벼운 puppeteer-core 사용
   };
