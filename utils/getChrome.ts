f05fa23cf49505aba3ccc36d8ad9c86863e89ae1
LEE GUKBEOM <tmwlak@gmail.com>
2025-07-10 08:15:07 +0900

    wip (5)

--- utils/getChrome.ts
+++ utils/getChrome.ts
@@ -1,5 +1,5 @@
 import chromium from "@sparticuz/chromium";
-import puppeteerCore from "puppeteer-core";
+// import puppeteerCore from "puppeteer-core";
 import puppeteer from "puppeteer";
 import puppeteerExtra from "puppeteer-extra";
 import puppeteerExtraPluginStealth from "puppeteer-extra-plugin-stealth";
@@ -12,7 +12,7 @@ export async function getChrome() {
 
   // vercel dev 로컬 환경
   if (process.env.VERCEL_REGION?.includes("dev")) {
-    console.log("Running in local (vercel dev) environment");
+    // console.log("Running in local (vercel dev) environment");
     return {
       executablePath: puppeteer.executablePath(), // puppeteer가 번들로 제공하는 Chromium 경로
       puppeteer: puppeteerExtra,
@@ -21,11 +21,11 @@ export async function getChrome() {
   }
 
   // (Production)
-  console.log("Running in Vercel production environment");
-  const executablePath = await chromium.executablePath();
+//   console.log("Running in Vercel production environment");
+//   const executablePath = await chromium.executablePath();
 
   return {
-    executablePath,
+    executablePath : await chromium.executablePath(),
     puppeteer: puppeteerExtra,
     // puppeteer: puppeteerCore, // 가벼운 puppeteer-core 사용
   };
